a4ed402858482dec2cf45d63221b913561a0f4eb
Mahmoud Mohamed <mahmoud.mmoawed@gmail.com>
2021-05-14 17:55:15 +0400

    HW1 files

---
+++ C Programming/Assignment 1/HW-1 - Ex1 Main.c
@@ -0,0 +1,26 @@
+/*
+ * Ex1 Main.c
+ *
+ *  Created on: May 14, 2021
+ *      Author: Mahmoud Mohamed
+ */
+
+
+//EX1:
+//
+//Write C Program to Print a Sentence
+//
+//i should see the Console as following:
+//##########Console-output###
+//C Programming
+//###########################
+//
+//########################################################################
+
+#include "stdio.h"
+
+int main ()
+{
+	printf("C Programming \n\r");
+	return 0;
+}
